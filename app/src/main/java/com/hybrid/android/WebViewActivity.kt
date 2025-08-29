ab69535ea4e84bb8a936e11c81ec1790b5275ec5
allen <allen.yho@yahoo.com>
2025-08-29 13:54:24 +0800

    注释

--- app/src/main/java/com/hybrid/android/WebViewActivity.kt
+++ app/src/main/java/com/hybrid/android/WebViewActivity.kt
@@ -70,12 +70,12 @@ class WebViewActivity : ComponentActivity() {
             prefs.edit { putBoolean("install_tracked", true) }
 
             // 发送全部事件。测试用
-            sendAFEvent(AFInAppEventType.LOGIN)
-            sendAFEvent(AFInAppEventType.COMPLETE_REGISTRATION)
-            sendAFEvent("Purchase")
-            sendAFEvent("OldRegPurchase")
-            sendAFEvent("TPFirstDeposit")
-            sendAFEvent("AddToCart")
+//            sendAFEvent(AFInAppEventType.LOGIN)
+//            sendAFEvent(AFInAppEventType.COMPLETE_REGISTRATION)
+//            sendAFEvent("Purchase")
+//            sendAFEvent("OldRegPurchase")
+//            sendAFEvent("TPFirstDeposit")
+//            sendAFEvent("AddToCart")
         }
 
         // 创建 FrameLayout 根容器
@@ -478,7 +478,7 @@ class WebViewActivity : ComponentActivity() {
 
 
     private fun showToast(text: String) {
-       Toast.makeText(this@WebViewActivity, text, Toast.LENGTH_LONG).show()
+       //Toast.makeText(this@WebViewActivity, text, Toast.LENGTH_LONG).show()
     }
 
     private fun handleApiResponse(apiUrl: String, fullRequestDataJson: String) {
